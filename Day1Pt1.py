dacca070f5c3afd50ed9fd2ac07aa79a4fb25d0c
anmaxwell <aniamaxwell@gmail.com>
2020-12-16 22:59:27 +0000

    day1 part1 solution

--- Day1Pt1.py
+++ Day1Pt1.py
@@ -1,13 +1,16 @@
 import csv
 
+#read in file from Elves and convert each value to int
 with open('number.csv', 'r') as csvfile:
     mylist = []
     for row in csv.reader(csvfile, delimiter=';'):
             mylist.append(int(row[0]))
 
+#take each number to check the remainder
 for i in mylist:
     remainder = 2020 - i
 
+    # see if the remainder is in the file and return the product
     if remainder in mylist:
         print(remainder*i)
         break
