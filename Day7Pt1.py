79ba0da4c4a3141ec2abb73dc6e6b6ffa49b1887
anmaxwell <aniamaxwell@gmail.com>
2020-12-26 19:29:18 +0000

    solution for day7 part1

--- Day7Pt1.py
+++ Day7Pt1.py
@@ -1,29 +1,37 @@
 #read in Rule
 output = open("Rules.txt", "r")
 
-#create list of responses
+#create a blank dictionary
+bag_dict = {}
+
+#set the bag colour to check for
+colourcheck = ['shiny gold']
+
+#create a list for the final count
+finalcount = []
+
+#create a dictionary of bags and remove unecessary characters
 for line in output:
     line=line.strip()
 
     bagrules = line.split("contain")
 
-    #create a blank dictionary
-    bag_dict = {}
-
-    mainbag=bagrules[0].replace(' bags', '')
+    mainbag=bagrules[0].rstrip(' bags')
     subbags=bagrules[1].split(',')
 
-    for singlebag in subbags:
-        if mainbag in bag_dict:
-            bag_dict[mainbag].append('singlebag')
-        else:
-            bag_dict[mainbag] = singlebag
+    for i in range(len(subbags)):
+        subbags[i]=subbags[i][3:].rstrip(' bags.')
 
-        
+    bag_dict[mainbag] = subbags
 
+#loop through to check each level of bags
+while len(colourcheck) != 0:
+    newcheck=[]
+    for item in bag_dict.items():
+        for colour in colourcheck:
+            if colour in item[1]:
+                newcheck.append(item[0])
+                finalcount.append(item[0])
+    colourcheck=newcheck
 
-
-    print(bag_dict)
-
-
-
+print(len(set(finalcount)))
