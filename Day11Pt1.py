385426b9861c1e77e3f03d09eee4a56c3e33366a
anmaxwell <aniamaxwell@gmail.com>
2020-12-31 21:55:51 +0000

    part attempt at day11 part2

--- Day11Pt1.py
+++ Day11Pt1.py
@@ -1,12 +1,23 @@
 #read in the floorplan
-output = open("floorplan.txt", "r")
+with open("floorplan.txt") as file:
+    output = [x for x in file.read().split()]
 
+#set row number, create seatplan and workout size of floorplan
+s = 0
+seatplan = []
+rowlen = len(output[0])
+collen = 0
 
+#add each seat to the seatplan with the seat no and state
+for row in output:
+    for seat in row:
+        seatplan.append([s,seat])
+        s +=1
+    collen +=1
 
-import Tkinter
-root = Tkinter.Tk(  )
-for r in range(3):
-   for c in range(4):
-      Tkinter.Label(root, text='R%s/C%s'%(r,c),
-         borderwidth=1 ).grid(row=r,column=c)
-root.mainloop(  )
+for seat in seatplan:
+    if seat[1] != '.':
+        seat[1] = 'A'
+
+print(seatplan, rowlen, collen)
+print('=======space===========')
